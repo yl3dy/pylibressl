37d2a299bfa143d76a60e7f01219e7a012f2ba50
Alexander Kiselyov <alexander.kiselyov@stonehenge-3.net.ru>
2017-02-06 01:56:57 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylibressl',
-    version='0.3',
+    version='0.4',
 
     packages=find_packages(),
     package_data={'pylibressl': ['libressl/lib/*.*']},
